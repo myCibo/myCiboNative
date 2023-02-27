451363bbae16ace180f1db4c07cee212bb32374e
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2023-02-27 10:21:49 -0800

    RecipeInstruction

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -2,19 +2,23 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import CustomButton from "../components/atoms/CustomButton"
 import RecipeDetails from "../components/molecules/RecipeDetails"
+import RecipeInstruction from '../components/organisms/RecipeInstruction';
+
+import data from "../recipeSteps.json";
+
+
+
 
 function HomeScreen() {
-
-  const handlePress = (event) => {
-    console.log("sjfkhfk");
-  }
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'  }}>
       <Text>Home Screen</Text>
      
+{/* 
           <RecipeDetails serving={5} timeInMinutes={30} calorie={234}/>
-          <CustomButton text="jaafhaejhf" onPress={handlePress}/>
+          <CustomButton text="jaafhaejhf" onPress={()=>console.log("presssed")}/> */}
+          <RecipeInstruction data= {data} />
 
 
     </View>
