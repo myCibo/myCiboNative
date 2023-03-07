8d95e0c64d98a6ed89110007765ee1a92dacbec6
Laurent Beique <lbeique@outlook.com>
2023-03-07 04:27:44 -0800

    styles
    i updated the styling on the fridge items and the options modal while learning how it works

--- src/components/organisms/OptionsModal.jsx
+++ src/components/organisms/OptionsModal.jsx
@@ -6,65 +6,75 @@ import {
   Image,
 } from "react-native";
 import Modal from "react-native-modal";
+import Icon from "../atoms/Icon";
+import Colors from "../../constants/styles";
 
 export default function OptionsModal({
   showOptions,
+  optionsType,
   onToggleModal,
   title,
-  options = [
-    { text: "option text", icon: "../../assets/images/list-icon.png" },
-  ],
+  // options = [
+  //   { text: "option text", icon: "../../assets/images/list-icon.png" },
+  // ],
   color,
 }) {
   const styles = StyleSheet.create({
+    modal: {
+      position: "absolute",
+      backgroundColor: Colors['creamyWhite'],
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      marginHorizontal: 0,
+      marginVertical: 0,
+      bottom: 100,
+      width: "100%",
+      // elevation: 0,
+    },
     optionRow: {
       alignItems: "center",
       justifyContent: "space-between",
       width: "100%",
       flexDirection: "row",
-      paddingLeft: 20,
-      paddingRight: 10,
-      justifyContent: "space-between", // Add this
+      paddingLeft: 32,
+      paddingRight: 32,
+      paddingBottom: 20,
+      paddingTop: 20,
+      borderBottomWidth: 1,
+      borderColor: Colors['white'],
     },
-    optionText: {
-      fontSize: 18,
-      fontWeight: "600",
-      padding: 16,
-    },
-    optionIcon: {
-      width: 24,
-      height: 24,
-      marginRight: 16,
-    },
-    modal: {
-      position: "absolute",
-      backgroundColor: "#E9E6E3",
-      overflow: "hidden",
-      marginHorizontal: 0,
-      bottom: "10%",
-      width: "100%",
-      paddingBottom: 16,
-      paddingTop: 16,
-    },
-    modalBackdrop: {
-      flex: 1,
-      justifyContent: "flex-end",
-      backgroundColor: "rgba(0, 0, 0, 0.5)",
+    optionLastRow: {
+      borderBottomWidth: 0,
     },
     title: {
       fontSize: 22,
       fontWeight: "bold",
-      paddingTop: 16,
     },
-    optionCancelIcon: {
+    optionText: {
+      fontSize: 18,
+      // fontWeight: "600",
+    },
+    optionRemoveText: {
+      color: Colors['primaryRed'],
+    },
+    optionIcon: {
       width: 24,
       height: 24,
-      marginRight: 16,
+    },
+    optionRemoveIcon: {
+      color: Colors['primaryRed'],
     },
   });
 
   const handleEditOptionPress = () => {
     console.log("Edit option pressed");
+
+
+    onToggleModal();
+  };
+
+  const handleUploadOptionPress = () => {
+    console.log("Upload option pressed");
     onToggleModal();
   };
 
@@ -82,43 +92,47 @@ export default function OptionsModal({
     <Modal
       isVisible={showOptions}
       onBackdropPress={onToggleModal}
-      backdropOpacity={0.5}
+      backdropOpacity={0.8}
       backdropTransitionOutTiming={0}
       style={styles.modal}
     >
       <View>
-        <TouchableWithoutFeedback onPress={handleDeleteOptionPress}>
+        <TouchableWithoutFeedback onPress={handleCancelOptionPress}>
           <View style={styles.optionRow}>
             <Text style={styles.title}>{title}</Text>
-            <TouchableWithoutFeedback onPress={handleDeleteOptionPress}>
-              <Image
-                source={require("../../assets/images/remove-icon.png")}
-                style={styles.optionCancelIcon}
-              />
+            <TouchableWithoutFeedback onPress={handleCancelOptionPress}>
+              <Icon name="close" size={32} color={Colors['primaryBlack']} />
             </TouchableWithoutFeedback>
           </View>
         </TouchableWithoutFeedback>
 
-        <TouchableWithoutFeedback onPress={handleEditOptionPress}>
-          <View style={styles.optionRow}>
-            <Text style={styles.optionText}>Edit</Text>
-            <TouchableWithoutFeedback onPress={handleEditOptionPress}>
-              <Image
-                source={require("../../assets/images/edit-icon.png")}
-                style={styles.optionIcon}
-              />
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
+        {optionsType === "ingredient" && (
+          <TouchableWithoutFeedback onPress={handleEditOptionPress}>
+            <View style={styles.optionRow}>
+              <Text style={styles.optionText}>edit</Text>
+              <TouchableWithoutFeedback onPress={handleEditOptionPress}>
+                <Icon name="edit" size={24} color={Colors['primaryBlack']} />
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+
+        {optionsType === "list" && (
+          <TouchableWithoutFeedback onPress={handleUploadOptionPress}>
+            <View style={styles.optionRow}>
+              <Text style={styles.optionText}>send a copy</Text>
+              <TouchableWithoutFeedback onPress={handleUploadOptionPress}>
+                <Icon name="upload" size={24} color={Colors['primaryBlack']} />
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        )}
 
         <TouchableWithoutFeedback onPress={handleDeleteOptionPress}>
-          <View style={styles.optionRow}>
-            <Text style={styles.optionText}>Remove</Text>
+          <View style={[styles.optionRow, styles.optionLastRow]}>
+            <Text style={[styles.optionText, styles.optionRemoveText]}>remove</Text>
             <TouchableWithoutFeedback onPress={handleDeleteOptionPress}>
-              <Image
-                source={require("../../assets/images/remove-icon.png")}
-                style={styles.optionIcon}
-              />
+              <Icon name="trash-x" size={32} color={Colors['primaryRed']} />
             </TouchableWithoutFeedback>
           </View>
         </TouchableWithoutFeedback>
