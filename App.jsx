480b68a33374dfa6f808f1a7751b0376809dea65
Hoang Cuong Nguyen <hcuong.ng.ca@gmail.com>
2023-04-13 17:18:05 -0700

    add google login

--- App.jsx
+++ App.jsx
@@ -5,12 +5,31 @@ import UserContext from './src/contexts/UserContext';
 import UserHandler from './src/handlers/UserHandler';
 import NavigationFooter from './src/navigation/NavigationFooter';
 import "react-native-url-polyfill/auto"
-
+import * as Google from 'expo-auth-session/providers/google';
+import axios from 'axios';
 const userHandler = new UserHandler();
 
 export default function App() {
   const [user, setUser] = useState(null);
+//   // WebBrowser.maybeCompleteAuthSession();
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: "552993921134-4lfc1ep9u4c3ud0h73tqtrhte75ldbtn.apps.googleusercontent.com",
+    androidClientId: '552993921134-tj76atrjgaav85pfoovenbgadoi5bnfa.apps.googleusercontent.com',
+    selectAccount: true,
+    // iosClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+  });
+  if (response?.type === 'success') {
+    (async () => {
+      const userData = await axios.get(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {
+          headers: { Authorization: `Bearer ${response.params.access_token}` },
+        }
+      );
 
+      console.log('user info:', userData)
+    })();
+  }
   useEffect(() => {
     const getUser = async () => {
       let storedUser = await AsyncStorage.getItem('user');
